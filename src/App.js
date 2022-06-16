133ae3d281feda33c3c2ffc3b89bc8145c067b36
root <root@HO2018100003.ichiho.net>
2022-06-16 14:07:06 +0900

    tei

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import xhtml from '../src/mock/test1/xhtml';
+// import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const editorConfiguration = {
+  toolbar: 
+     [
+        'undo', 'redo',
+        '|',
+        'exportPdf', 'exportWord',
+        '|',
+        'wproofreader', 'findAndReplace', 'selectAll',
+        '|',
+        'heading',
+        '|',
+        'removeFormat', 'bold', 'italic', 'strikethrough', 'underline', 'code', 'subscript', 'superscript',
+        '|',
+        'specialCharacters', 'horizontalLine', 'pageBreak',
+        '|',
+        '-',
+        'highlight', 'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor',
+        '|',
+        'link', 'blockQuote', 'insertTable', 'uploadImage', 'mediaEmbed', 'codeBlock', 'htmlEmbed',
+        '|',
+        'bulletedList', 'numberedList',
+        '|',
+        'outdent', 'indent', 'alignment',
+        '|',
+        'sourceEditing'
+    ]
+};
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <CKEditor
+                    editor={ Editor }
+                    config={ editorConfiguration }
+                    data={xhtml.data}
+                    onReady={ editor => {
+                      editor.ui.getEditableElement().parentElement.insertBefore(
+                        editor.ui.view.toolbar.element,
+                        editor.ui.getEditableElement()
+                      );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                    onError={ ( error, { willEditorRestart } ) => {
+                        if ( willEditorRestart ) {
+                            this.editor.ui.view.toolbar.element.remove();
+                        }
+                    } }
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
